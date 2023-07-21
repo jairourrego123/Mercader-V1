107348637e5a34f6661679b82b6e46b8dfcf93c1
unknown <jairourrego123@gmail.com>
2023-07-21 14:43:25 -0500

    Change  name find fields of entity Users

--- Backend/src/main/java/com/elmercader/catalogo/repositories/UserCRUDRepository.java
+++ Backend/src/main/java/com/elmercader/catalogo/repositories/UserCRUDRepository.java
@@ -4,13 +4,12 @@ import com.elmercader.catalogo.models.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserCRUDRepository  extends CrudRepository<User,Integer> {
-    public Optional<User> findByUserEmail(String userEmail);
+    public Optional<User> findByEmail(String userEmail);
 
-    @Query(value = "SELECT * FROM users WHERE userEmail=? AND userPassword=?;",nativeQuery = true)
+    @Query(value = "SELECT * FROM users WHERE email=? AND password=?;",nativeQuery = true)
     public Optional<User> validateLogin(String userEmail,String userPassword);
 
 
--- Backend/src/main/java/com/elmercader/catalogo/repositories/UserRepository.java
+++ Backend/src/main/java/com/elmercader/catalogo/repositories/UserRepository.java
@@ -4,7 +4,6 @@ import com.elmercader.catalogo.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 @Repository
@@ -26,7 +25,7 @@ public class UserRepository {
      * @return Optional User
      */
     public Optional<User> getByEmail(String email){
-        return userCRUDRepository.findByUserEmail(email);
+        return userCRUDRepository.findByEmail(email);
     }
 
     /**
